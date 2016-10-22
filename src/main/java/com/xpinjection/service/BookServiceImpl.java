1e60def3f1fa42ee9b777f3c00f4c6ea71a11a8f
Mikalai Alimenkou <mikalai.alimenkou@epam.com>
2016-10-22 14:18:14 +0300

    added validation and correction logic to book service

--- src/main/java/com/xpinjection/service/BookServiceImpl.java
+++ src/main/java/com/xpinjection/service/BookServiceImpl.java
@@ -11,13 +11,20 @@ package com.xpinjection.service;
 
 import com.xpinjection.dao.BookDao;
 import com.xpinjection.domain.Book;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+import static org.apache.commons.lang3.StringUtils.splitByCharacterTypeCamelCase;
 
 /**
  * @author Alimenkou Mikalai
@@ -27,6 +34,7 @@ import static java.util.stream.Collectors.toList;
 @Transactional
 public class BookServiceImpl implements BookService {
     private final BookDao bookDao;
+    private final ConcurrentMap<String, List<Book>> cache = new ConcurrentHashMap<>();
 
     public BookServiceImpl(BookDao bookDao) {
         this.bookDao = bookDao;
@@ -42,11 +50,19 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public List<Book> findBooksByAuthor(String author) {
-        return bookDao.findByAuthor(author);
+        Validate.notBlank(author, "Author is empty!");
+        String correctAuthor = splitFirstAndLastName(author);
+        return cache.computeIfAbsent(correctAuthor, bookDao::findByAuthor);
     }
 
     @Override
     public List<Book> findAllBooks() {
         return bookDao.findAll();
+    }
+
+    private String splitFirstAndLastName(String author) {
+        return Arrays.stream(splitByCharacterTypeCamelCase(author))
+                .filter(StringUtils::isNotBlank)
+                .collect(joining(" "));
     }
 }
--- src/test/java/com/xpinjection/service/BookServiceImplTest.java
+++ src/test/java/com/xpinjection/service/BookServiceImplTest.java
@@ -21,10 +21,12 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.util.Collections.emptyList;
+import static org.codehaus.groovy.runtime.InvokerHelper.asList;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.notNull;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * @author Alimenkou Mikalai
@@ -57,5 +59,59 @@ public class BookServiceImplTest {
         books.put("The first", "author");
         books.put("The second", "another author");
         assertThat(bookService.addBooks(books), hasItems(first, second));
+    }
+
+    @Test
+    public void ifNoBooksFoundForAuthorReturnEmptyList() {
+        when(dao.findByAuthor("a")).thenReturn(emptyList());
+
+        assertNoBooksFound("a");
+        verify(dao, only()).findByAuthor("a");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ifAuthorIsEmptyThrowException() {
+        bookService.findBooksByAuthor(" \t \n ");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void ifAuthorIsNullThrowException() {
+        bookService.findBooksByAuthor(null);
+    }
+
+    @Test
+    public void booksByAuthorShouldBeCached() {
+        Book book = new Book("The book", "author");
+        when(dao.findByAuthor("a")).thenReturn(asList(book));
+        when(dao.findByAuthor("a a")).thenReturn(emptyList());
+
+        assertBooksByAuthor("a", book);
+        assertBooksByAuthor("a", book);
+        assertNoBooksFound("a a");
+        verify(dao, times(1)).findByAuthor("a");
+    }
+
+    @Test
+    public void ifCamelCaseDetectedThenSplitInvalidAuthorNameOnFirstAndLastName() {
+        Book book = new Book("The book", "Mikalai Alimenkou");
+        when(dao.findByAuthor("Mikalai Alimenkou")).thenReturn(asList(book));
+
+        assertBooksByAuthor("MikalaiAlimenkou", book);
+    }
+
+    @Test
+    public void authorNameShouldBeTrimmedBeforeUsage() {
+        Book book = new Book("The book", "Mikalai Alimenkou");
+        when(dao.findByAuthor("Mikalai Alimenkou")).thenReturn(asList(book));
+
+        assertBooksByAuthor(" \t Mikalai \n Alimenkou \t ", book);
+    }
+
+    private void assertBooksByAuthor(String author, Book book) {
+        assertThat(bookService.findBooksByAuthor(author), hasItem(book));
+    }
+
+    private void assertNoBooksFound(String author) {
+        assertThat(bookService.findBooksByAuthor(author), is(empty()));
     }
 }
