74fad7170ecd14d169c440132f958b4b6d2b9c59
Mikalai Alimenkou <mikalai.alimenkou@epam.com>
2017-07-11 01:34:41 +0300

    added data set usage for system level test

--- src/test/java/com/xpinjection/web/BookControllerRestAssuredSystemTest.java
+++ src/test/java/com/xpinjection/web/BookControllerRestAssuredSystemTest.java
@@ -1,21 +1,34 @@
 package com.xpinjection.web;
 
+import com.github.database.rider.core.DBUnitRule;
+import com.github.database.rider.core.api.dataset.DataSet;
 import org.apache.http.HttpStatus;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
 
+import javax.sql.DataSource;
 import java.net.URI;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class BookControllerRestAssuredSystemTest {
+    @Autowired
+    private WebApplicationContext context;
+
+    @Rule
+    public DBUnitRule dbUnitRule = DBUnitRule.instance(() -> context.getBean(DataSource.class).getConnection());
+
     @Test
+    @DataSet("default-books.xml")
     public void allBooksFromDatabaseAreAvailableOnWeb() throws Exception {
         given()
             .accept("text/html;charset=UTF-8")
@@ -25,7 +38,7 @@ public class BookControllerRestAssuredSystemTest {
             .statusCode(HttpStatus.SC_OK)
             .contentType("text/html;charset=UTF-8")
             .content(allOf(
-                containsString("Spring in Action, <em>Who knows?</em>"),
-                containsString("Hibernate in Action, <em>Who cares?</em>")));
+                containsString("Spring in Action, <em>Craig Walls</em>"),
+                containsString("Hibernate in Action, <em>Christian Bauer</em>")));
     }
 }
